c820032c670589377f56d155bc7aa469c46ac606
Kimberly Robinson <158204628+kimberlyrobinson11122@users.noreply.github.com>
2024-07-07 15:12:22 -0700

    minor comment update, nothing notable

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -3,7 +3,7 @@ const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
-// Initialize Product model (table) by extending off Sequelize's Model class
+// this will nitialize Product model (table) by extending off Sequelize the model class
 class Product extends Model {}
 
 // set up fields and rules for Product model
